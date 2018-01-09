db87f0e883f218769e6c36a41396e29fccd82769
howard <hpf@ruiboyun.net>
2018-01-09 16:32:29 +0800

    [test] change test ui, hide some elements.

--- src/webrtc.js
+++ src/webrtc.js
@@ -11,7 +11,7 @@ function WebRTC(opts) {
             debug: false,
             // makes the entire PC config overridable
             peerConnectionConfig: {
-                iceServers: [{'urls': 'stun:stun.l.google.com:19302'}]
+                iceServers: [{'urls': 'stun:119.29.35.83:3499'}]
             },
             peerConnectionConstraints: {
                 optional: []
